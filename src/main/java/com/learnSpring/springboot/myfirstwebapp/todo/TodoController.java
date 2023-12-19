3f53f8de0f56f8ce8f5f76f89ddadc2c32559454
TriBhaskar <panthribhaskar@gmail.com>
2023-12-19 22:51:10 +0530

    update and delete

--- src/main/java/com/learnSpring/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/learnSpring/springboot/myfirstwebapp/todo/TodoController.java
@@ -44,4 +44,27 @@ public class TodoController {
         todoService.addTodo(username,todo.getDescription(),todo.getTargetDate(),false);
         return "redirect:list-todos";
     }
+
+    @RequestMapping("delete-todo")
+    public String deleteTodos(@RequestParam int id){
+        todoService.deleteById(id);
+        return "redirect:list-todos";
+    }
+    @RequestMapping(value = "update-todo", method = RequestMethod.GET)
+    public String updateTodo(@RequestParam int id, ModelMap model){
+        Todo todo = todoService.findById(id);
+        model.addAttribute("todo",todo);
+        return "todo";
+    }
+    @RequestMapping(value="update-todo",method = RequestMethod.POST)
+    public String showUpdateTodoPage(ModelMap model, @Valid Todo todo, BindingResult result){
+        if(result.hasErrors()){
+            return "todo";
+        }
+        String username =(String) model.get("name");
+        todo.setUsername(username);
+        todoService.updateTodo(todo);
+        return "redirect:list-todos";
+    }
+
 }
--- src/main/java/com/learnSpring/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/learnSpring/springboot/myfirstwebapp/todo/TodoService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+
 @Service
 public class TodoService {
     private static List<Todo> todos= new ArrayList<>();
@@ -23,6 +25,22 @@ public class TodoService {
 
     public void addTodo(String username, String description, LocalDate targetDate, boolean done){
         Todo todo = new Todo(++todosCount,username,description,targetDate,done);
+        todos.add(todo);
+    }
+    public void deleteById(int id){
+        //todo -> todo.getId() == id;
+        Predicate<? super Todo> predicate= todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate= todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+    }
+
+    public void updateTodo(Todo todo) {
+        deleteById(todo.getId());
         todos.add(todo);
     }
 }
